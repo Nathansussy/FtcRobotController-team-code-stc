831af7f52f32c1b26789e63969400292a990df8c
Nathansussy <nathansussy1987@gmail.com>
2025-06-18 11:44:10 -0700

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleOp.java
@@ -2,17 +2,28 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class FirstTeleOp extends OpMode {
+
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+
     @Override
     public void init() {
+        leftMotor = hardwareMap.get(DcMotor.class, "left");
+        rightMotor = hardwareMap.get(DcMotor.class, "right");
 
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
+        double leftPower = -gamepad1.left_stick_y;
+        double rightPower = -gamepad1.right_stick_y;
+
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
 
     }
-}
-hjundoh ngh9sdvjoi09dgfwe i0
